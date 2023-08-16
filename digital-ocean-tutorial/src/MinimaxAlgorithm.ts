fe420b04a463145ffc5a221e2b2b7e36845717b7
Vansh jhunjhunwala <vansh.jhunjhunwala@ghyston.com>
2023-08-16 21:05:50 +0100

    shitty algorithm structure

---
+++ digital-ocean-tutorial/src/MinimaxAlgorithm.ts
@@ -0,0 +1,135 @@
+﻿
+
+
+
+// -1 for c piece, -2 for C king
+
+export const find_available_moves = (board: Array<Array<number>>) => {
+    const available_moves: Array<Array<number>> = []
+    const available_jumps = []
+    for (var row = 0; row < 8; row++) {
+        for (var column = 0; column < 8; column++) {
+            if(board[row][column] === -1) {
+                if (check_moves(board, row, column, row + 1, column + 1)){
+                    available_moves.push([row, column, row + 1, column + 1])
+                }
+                if (check_moves(board, row, column, row + 1, column - 1)){
+                    available_moves.push([row, column, row + 1, column - 1])
+                }
+                if (check_jumps(board, row, column, row + 1, column - 1, row + 2, column - 2)){
+                    available_jumps.push([row, column, row + 2, column - 2])
+                }
+                if (check_jumps(board, row, column, row + 1, column + 1, row + 2, column + 2)){
+                    available_jumps.push([row, column, row + 2, column + 2])
+                }
+            }
+            else if(board[row][column] === -2) {
+                if (check_moves(board, row, column, row + 1, column + 1)){
+                    available_moves.push([row, column, row + 1, column + 1])
+                }
+                if (check_moves(board, row, column, row + 1, column - 1)){
+                    available_moves.push([row, column, row + 1, column - 1])
+                }
+                if (check_moves(board, row, column, row - 1, column - 1)){
+                    available_moves.push([row, column, row - 1, column - 1])
+                }
+                if (check_moves(board, row, column, row - 1, column - 1)){
+                    available_moves.push([row, column, row - 1, column - 1])
+                }
+                if (check_jumps(board, row, column, row + 1, column - 1, row + 2, column - 2)){
+                    available_jumps.push([row, column, row + 2, column - 2])
+                }
+                if (check_jumps(board, row, column, row + 1, column + 1, row + 2, column + 2)){
+                    available_jumps.push([row, column, row + 2, column + 2])
+                }
+                if (check_jumps(board, row, column, row + 1, column - 1, row + 2, column - 2)){
+                    available_jumps.push([row, column, row + 2, column - 2])
+                }
+                if (check_jumps(board, row, column, row + 1, column + 1, row + 2, column + 2)){
+                    available_jumps.push([row, column, row + 2, column + 2])
+                }
+            }
+
+        }
+    }
+    if(available_jumps.length == 0)
+        return available_jumps
+    else
+        return available_moves
+
+}
+
+export const check_moves = (board, rowFrom, columnFrom, rowTo, columnTo) => {
+    if (rowTo > 7 || rowTo < 0) {
+        return false
+    }
+    if (columnTo > 7 || columnTo < 0) {
+        return false
+    }
+    if(board[rowFrom][columnFrom] == 0) {
+        return false
+    }
+    if(board[rowTo][columnTo] != 0) {
+        return false
+    }
+    if(board[rowFrom][columnFrom] == 1 || board[rowFrom][columnFrom] == 2) {
+        return false
+    }
+    if(board[rowTo][columnTo] == 0) {
+        return true
+    }
+}
+
+export const check_jumps = (board, rowFrom, columnFrom, rowVia, columnVia, rowTo, columnTo) => {
+    if(rowTo > 7 || rowTo < 0)
+        return false
+    if(columnTo > 7 || columnTo < 0)
+        return false
+    if(board[rowVia][columnVia] == 0)
+        return false
+    if(board[rowVia][columnVia] == -2 || board[rowVia][columnVia] == -1)
+        return false
+    if(board[rowTo][columnTo] != 0)
+        return false
+    if(board[rowFrom][columnFrom] == 0)
+        return false
+    if(board[rowFrom][columnFrom] == 1 || board[rowFrom][columnFrom] == 2)
+        return false
+    return true
+}
+
+
+const minimax = (board, depth, alpha, beta, maximizing_player, mandatory_jumping) => {
+    if(depth == 0) {
+        return calculateHeuristics(board)
+    }
+    const currentStates = structuredClone(board)
+}
+
+
+const calculateHeuristics = (board) => {
+
+}
+
+
+   /* current_state = Node(deepcopy(board))
+    if maximizing_player is True:
+        max_eval = -math.inf
+    for child in current_state.get_children(True, mandatory_jumping):
+    ev = Checkers.minimax(child.get_board(), depth - 1, alpha, beta, False, mandatory_jumping)
+    max_eval = max(max_eval, ev)
+    alpha = max(alpha, ev)
+    if beta <= alpha:
+    break
+    current_state.set_value(max_eval)
+    return max_eval
+    else:
+    min_eval = math.inf
+    for child in current_state.get_children(False, mandatory_jumping):
+    ev = Checkers.minimax(child.get_board(), depth - 1, alpha, beta, True, mandatory_jumping)
+    min_eval = min(min_eval, ev)
+    beta = min(beta, ev)
+    if beta <= alpha:
+    break
+    current_state.set_value(min_eval)
+    return min_eval*/
---
+++ digital-ocean-tutorial/src/Node.ts
@@ -0,0 +1,84 @@
+﻿import { find_available_moves, check_moves} from "./MinimaxAlgorithm";
+
+
+class GameHelper {
+
+    set parent(value) {
+        this._parent = value;
+    }
+    get value() {
+        return this._value;
+    }
+    get parent() {
+        return this._parent;
+    }
+    get move() {
+        return this._move;
+    }
+    get board(): number[][] {
+        return this._board;
+    }
+
+    private _board: number[][]
+    private _move: null | number[]
+    private _parent
+    private _value
+
+    constructor(board, move, parent, value) {
+        this._board = board
+        this._move = null
+        this._value = null
+        this._parent = null
+    }
+
+    get_children = (minimizing_player) => {
+        const current_state = structuredClone(this.board)
+        const available_moves = []
+        const children_states = []
+        if(minimizing_player) {
+        }
+    }
+
+}
+
+
+/*
+
+
+current_state = deepcopy(self.board)
+
+big_letter = ""
+queen_row = 0
+if minimizing_player is True:
+    available_moves = Checkers.find_available_moves(current_state, mandatory_jumping)
+big_letter = "C"
+queen_row = 7
+else:
+available_moves = Checkers.find_player_available_moves(current_state, mandatory_jumping)
+big_letter = "B"
+queen_row = 0
+for i in range(len(available_moves)):
+old_i = available_moves[i][0]
+old_j = available_moves[i][1]
+new_i = available_moves[i][2]
+new_j = available_moves[i][3]
+state = deepcopy(current_state)
+Checkers.make_a_move(state, old_i, old_j, new_i, new_j, big_letter, queen_row)
+children_states.append(Node(state, [old_i, old_j, new_i, new_j]))
+return children_states
+
+def set_value(self, value):
+self.value = value
+
+def get_value(self):
+return self.value
+
+def get_board(self):
+return self.board
+
+def get_parent(self):
+return self.parent
+
+def set_parent(self, parent):
+self.parent = parent
+*/
